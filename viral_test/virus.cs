a1c904afccb23af31fe31f2f8efbfad30af0bcfe
chris forst <0xicl33n@gmail.com>
2015-01-01 20:19:37 -0800

    legit virus work begun

--- viral_test/virus.cs
+++ viral_test/virus.cs
@@ -1,5 +1,7 @@
+//Space Engineers proof of concept virus for ships - version .1 - Codename Shi'ido
 List<IMyTerminalBlock> batteries = new List<IMyTerminalBlock> ();
 List<IMyTerminalBlock> reactors = new List<IMyTerminalBlock> ();
+List<IMyTerminalBlock> cockpits = new List<IMyTerminalBlock>();
 
 void Main() {
 	reactors.Clear();
@@ -7,12 +9,23 @@ void Main() {
 
     GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batteries);
     GridTerminalSystem.GetBlocksOfType<IMyReactor>(reactors); 
-    
-    //for (int i = 0; i < batteries.Count; i++) {
-    	//batteries[i].GetActionWithName("OnOff").Apply(batteries[i]);
-
+    //batteries off
+    for (int i = 0; i < batteries.Count; i++) {
+    	batteries[i].GetActionWithName("OnOff").Apply(batteries[i]);
+    //reactors off
     for (int i = 0; i < reactors.Count; i++) {
-       batteries[i].GetActionWithName("OnOff").Apply(reactors[i]);
-
+       reactors[i].GetActionWithName("OnOff").Apply(reactors[i]);
+   }
+   //cockpits
+    GridTerminalSystem.GetBlocksOfType<Icockpits>(cockpits);
+    int CockpitCount = cockpits.Count;
+    int AlarmCount = Alarms.Count;
+    for(int i = 0; i < CockpitCount; i++) {
+    	//turnoff dampeners on all cockpits
+        ITerminalAction toggleDampeners = cockpits[i].GetActionWithName("DampenersOverride");
+        toggleDampeners.Apply(cockpits[i]);
+        //turnoff thruster control on all cockpits
+        ITerminalAction toggleThrusters = cockpits[i].GetActionWithName("ControlThrusters");
+		toggleThrusters.Apply(cockpits[i]);
     }
 }
