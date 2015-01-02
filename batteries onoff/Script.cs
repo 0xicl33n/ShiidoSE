964d8cf5995578cc9d921d3cf7cdeb42944d86d9
chris forst <0xicl33n@gmail.com>
2015-01-01 19:29:03 -0800

    added reactor control

--- batteries onoff/Script.cs
+++ batteries onoff/Script.cs
@@ -1,12 +1,18 @@
-    List<IMyTerminalBlock> batteries = new List<IMyTerminalBlock> ();
+List<IMyTerminalBlock> batteries = new List<IMyTerminalBlock> ();
+List<IMyTerminalBlock> reactors = new List<IMyTerminalBlock> ();
 
 void Main() {
-
+	reactors.Clear();
     batteries.Clear();
 
-    GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batteries);    
+    GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batteries);
+    GridTerminalSystem.GetBlocksOfType<IMyReactor>(reactors); 
+    
+    //for (int i = 0; i < batteries.Count; i++) {
+    	//batteries[i].GetActionWithName("OnOff").Apply(batteries[i]);
 
-    for (int i = 0; i < batteries.Count; i++) {
-       batteries[i].GetActionWithName("OnOff").Apply(batteries[i]);
+    for (int i = 0; i < reactors.Count; i++) {
+       batteries[i].GetActionWithName("OnOff").Apply(reactors[i]);
+
     }
 }
